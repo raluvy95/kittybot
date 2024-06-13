ac0f920031d4846c5bbb4eb7a69bdafa30c9211f
raluvy95 <ralucadoge069@gmail.com>
2024-06-13 23:35:45 +0300

    added random char

---
+++ src/cmd/utils/randomchar.ts
@@ -0,0 +1,25 @@
+import { randomChar } from "../../lib/char";
+import { Command } from "../../types";
+
+export const cmd: Command = {
+    name: "randomchar",
+    description: "Randomize random unicode chars",
+    ownerOnly: true,
+    async run(ctx) {
+        let arg: number;
+        if (!ctx.match) {
+            arg = 32;
+        } else {
+            arg = Number(ctx.match)
+        }
+        if (Number.isNaN(arg)) {
+            arg = 32;
+        } else if (arg > 128) {
+            arg = 128;
+        } else if (arg < 1) {
+            arg = 1;
+        }
+        const result = randomChar(arg)
+        ctx.reply(result);
+    },
+}
---
+++ src/lib/char.ts
@@ -0,0 +1,10 @@
+export function randomChar(length: number) {
+    const chars = [];
+    for (let i = 0; i < length; i++) {
+        const unicode_latin = Math.floor(Math.random() * 0x2AF) + 20;
+        const char = String.fromCodePoint(unicode_latin)
+        chars.push(char);
+    }
+
+    return Buffer.from(chars.join(""), "utf-8").toString();
+}
