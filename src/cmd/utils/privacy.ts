49b0eecc05aaa4975f4ea3993d5c10e25e28f938
raluvy95 <ralucadoge069@gmail.com>
2024-07-03 23:00:36 +0300

    added privacy policy

---
+++ src/cmd/utils/privacy.ts
@@ -0,0 +1,15 @@
+import { withHTMLmarkdown } from "../../lib/utils";
+import { Command } from "../../types";
+
+export const cmd: Command = {
+    name: "privacy",
+    description: "Privacy Policy",
+    async run(ctx) {
+        ctx.reply(`<b>Privacy Policy</b>
+
+        We don't collect any required data and we don't sell your personal information, your messages and the data you provided while using our commands.
+        We only store your user id and your Last.fm name in our database when you are using /register command (so therefore opt-in). You can delete your data on request by using /unregister
+        
+        This bot is <a href="https://github.com/raluvy95/kittybot">open source</a> so no hidden data collection.`, withHTMLmarkdown());
+    },
+}
